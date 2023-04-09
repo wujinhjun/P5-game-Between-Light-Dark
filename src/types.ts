52a47c4278c098784ecfcdf567dd297cc2635a82
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-04-09 21:26:39 +0800

    feat: tyoes

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,36 @@
+import p5 from "p5";
+import QuadTree from "./helper/QuadTree";
+
 export interface IPoint {
   x: number;
   y: number;
-  userData: string;
+  readonly userData?: string;
 }
+
+export interface IRectangle {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}
+
+export interface ICicle {
+  x: number;
+  y: number;
+  r: number;
+  rSquared: number;
+}
+
+export interface IQuad {
+  boundary: IRectangle;
+  capacity: number;
+  depth: number;
+  points: IPoint[];
+  divided: boolean;
+  northeast: QuadTree;
+  northwest: QuadTree;
+  southeast: QuadTree;
+  southwest: QuadTree;
+}
+
+export type Vector = p5.Vector;
